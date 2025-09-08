c5db2c82439149dcf08f45ba6841bffd660b47f4
Mladen Branković <root@brankovic.dev>
2025-09-08 16:31:49 +0200

    maintainers: add imatpot

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -10895,6 +10895,12 @@
     githubId = 993484;
     name = "Greg Hale";
   };
+  imatpot = {
+    email = "nixpkgs@brnk.vc";
+    github = "imatpot";
+    githubId = 39416660;
+    name = "Mladen Branković";
+  };
   imgabe = {
     email = "gabrielpmonte@hotmail.com";
     github = "ImGabe";
